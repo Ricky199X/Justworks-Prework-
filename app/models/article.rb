f6efc844899afc94aabcbda31240f6bae9357139
Ricky199X <rickyrojas19@gmail.com>
2020-10-31 13:27:16 -0400

    adds comments model + set up article association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ApplicationRecord
-    # validations - validates title + length of title
+    # active record association with comment model
+    has_many :comments
 
+    # validations - validates title + length of title
     validates :title, presence: true,  
         length: { minimum: 5 }
     
+        
     
 end
