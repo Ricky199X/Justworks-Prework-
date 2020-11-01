a900f4b65bc1b03987ef087b37773d3984b9af78
Ricky199X <rickyrojas19@gmail.com>
2020-11-01 16:14:50 -0500

    add basic authentication to destroy comments and change articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     # active record association with comment model
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
     # validations - validates title + length of title
     validates :title, presence: true,  
