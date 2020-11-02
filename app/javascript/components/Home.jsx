544adb4c02de4232f3a946716d9c70121b5cc657
Ricky199X <rickyrojas19@gmail.com>
2020-11-01 22:12:42 -0500

    adds react

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+    <div className="home">
+        <h1>Hello This is the new home page</h1>
+    </div>
+);
